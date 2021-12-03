9b2f2e63fd3de5d54eca107cfc0a433914599582
Rane <wyatt.karnes@gmail.com>
2021-12-03 15:02:05 -0700

    Messing with the grid-marginal success.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const gallery = document.getElementById('gallery');
 const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('selectedImage');
-const imageIndexes = [0,1,2,3,4,5,6,7,8];
+const imageIndexes = [0,1,2,3,4,5,6,7,];
 const selectedIndex = null;
 
 imageIndexes.forEach(i => {
