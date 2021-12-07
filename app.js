0c53186f57a34f87ff7fb56f8480f175e937413b
Wyatt <wyatt.karnes@gmail.com>
2021-12-06 18:40:46 -0700

    Basic script to make wider pictures bigger.
    - if a picture is wider than a set amount, it spans two grid columns instead of 1.

--- app.js
+++ app.js
@@ -3,16 +3,24 @@ const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('selectedImage');
 const imageIndexes = [0,1,2,3,4,5,6,7,8];
 const selectedIndex = null;
+var prevImage = null;
 
 imageIndexes.forEach(i => {
     const image = document.createElement('img');
     image.src = `/Images/Gallery/TAT2021/TAT21_${i}.jpg`;
     image.alt = `TAT Picture ${i+1}`;
     image.classList.add('galleryImg');
+ 
+    //you can check image.naturalHeight, for aspect ratio specific stuff.
+    if(prevImage != null && image.naturalWidth > prevImage.naturalWidth){
+        image.classList.add('wider');
+    }
 
     image.addEventListener("click", ()=>{
         //popup things
     })
+
+    prevImage = image;
 
     gallery.appendChild(image);
 })
